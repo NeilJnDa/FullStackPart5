840133d61d55cab2c77a51cb82c050576b6339ef
Jinda <neil_jnda@outlook.com>
2022-10-05 19:36:13 +0300

    5.7

--- bloglist-frontend-main/src/components/Blog.js
+++ bloglist-frontend-main/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Toggle from "./Toggle"
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = {display: visible ? 'none': ''}
+  const showWhenVisible = {display: visible ? '': 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  if(!visible){
+    return(
+      <div style = {blogStyle}>
+      {blog.title} {blog.author} <button onClick={()=>setVisible(true)}>View</button>
+      </div>  
+    )
+  }
+  else return(
+    <div style = {blogStyle}>
+      <div>
+        {blog.title} <button onClick={()=>setVisible(false)}>Hide</button>
+      </div>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        Likes {blog.likes} <button>Like</button>
+      </div>
+      <div>
+        {blog.author} 
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
